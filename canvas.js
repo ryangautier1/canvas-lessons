0ea7ad5a00a9e6c8d9aa4766f284e8d9f3561cbf
Ryan Gautier <ryangautier2@gmail.com>
2020-08-31 17:42:20 -0500

    lesson 2

--- canvas.js
+++ canvas.js
@@ -6,4 +6,27 @@ canvas.height = window.innerHeight;
 // context
 let c = canvas.getContext("2d");
 
-c.fillRect(100, 100, 100, 100)
+c.fillStyle = 'rgba(255,0,0,0.1)';
+c.fillRect(100, 100, 100, 100);
+
+// line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 200);
+c.strokeStyle = '#a3a3a3';
+c.stroke();
+
+// arc
+// c.beginPath();
+// c.arc(300,300, 30, 0, Math.PI*2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+
+for (let i=0; i<1000; i++) {
+  c.beginPath();
+
+  c.arc(Math.random()*window.innerWidth,Math.random()*window.innerHeight, 5, 0, Math.PI*2, false);
+  c.strokeStyle = 'blue';
+  c.stroke();
+}
